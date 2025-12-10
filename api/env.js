553603f5dd71d72a57d77ec0f167d1626630bd19
Evaldas Visockas <vievaldas@gmail.com>
2025-12-10 23:12:55 +0200

    Add shoot delay as environment variable

--- api/env.js
+++ api/env.js
@@ -1,3 +1,3 @@
 export default function handler(req, res) {
-  res.json({ BALL_SPEED: process.env.BALL_SPEED });
+  res.json({ BALL_SPEED: process.env.BALL_SPEED, SHOOT_DELAY: process.env.SHOOT_DELAY });
 }
